db234506552f433e56404a5a44674e7bcae8e5fc
Ishtiyaq1007 <ishtiyaq1007@gmail.com>
2026-09-25 20:43:38 +0530

    Create SeatDuplicationChecker.java

---
+++ Assignment probs/SeatDuplicationChecker.java
@@ -0,0 +1,31 @@
+public class SeatDuplicationChecker {
+
+    // Method to check for duplicate seat numbers
+    public static void checkDuplicateSeats(int[] seatNumbers) {
+        boolean duplicateFound = false;
+
+        for (int i = 0; i < seatNumbers.length; i++) {
+            for (int j = i + 1; j < seatNumbers.length; j++) {
+                if (seatNumbers[i] == seatNumbers[j]) {
+                    System.out.println("Duplicate Seat Number Found: " + seatNumbers[i]);
+                    duplicateFound = true;
+                }
+            }
+        }
+
+        if (!duplicateFound) {
+            System.out.println("No Duplicate Seats Found");
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] seats1 = {101, 102, 103, 102, 105};
+        int[] seats2 = {101, 102, 103, 104, 105};
+
+        System.out.println("Checking seats1:");
+        checkDuplicateSeats(seats1);
+
+        System.out.println("\nChecking seats2:");
+        checkDuplicateSeats(seats2);
+    }
+}
